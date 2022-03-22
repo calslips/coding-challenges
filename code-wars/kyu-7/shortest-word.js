777b8e4886ecab81ce3ce0f5d3c7cd64fc549ede
Caleb Phillips <csphillips5@gmail.com>
2022-03-21 21:01:39 -0400

    submit extra coding challenge

---
+++ code-wars/kyu-7/shortest-word.js
@@ -0,0 +1,9 @@
+/**
+ * Simple, given a string of words, return the length of the shortest word(s).
+
+String will never be empty and you do not need to account for different data types.
+ */
+
+function findShort(s){
+  return s.split(' ').reduce((count, word) => word.length < count ? word.length : count, s.length);
+}
