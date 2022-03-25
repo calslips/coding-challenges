06767117607afad8cda5ff79935260a69496abdf
Caleb Phillips <csphillips5@gmail.com>
2022-03-25 13:10:54 -0400

    submit extra coding challenge

---
+++ code-wars/kyu-8/fake-binary.js
@@ -0,0 +1,16 @@
+/**
+ * Given a string of digits, you should replace any digit below 5 with '0'
+ * and any digit 5 and above with '1'. Return the resulting string.
+
+Note: input will never be an empty string
+ */
+
+function fakeBin(x){
+  let bin = '';
+  for (let i = 0; i < x.length; i++) {
+    x[i] < 5 ? bin += 0 : bin += 1;
+  }
+  return bin;
+}
+
+// const fakeBin = x => x.split('').map(n => n < 5 ? 0 : 1).join('');
