6a6aecff9d579402177d4f56e3ed95ec68c099f5
Caleb Phillips <csphillips5@gmail.com>
2022-04-04 20:59:27 -0400

    submit extra coding challenge

---
+++ code-wars/kyu-6/split-strings.js
@@ -0,0 +1,19 @@
+/**
+ * Complete the solution so that it splits the string into pairs of two characters.
+ * If the string contains an odd number of characters then it should replace the
+ * missing second character of the final pair with an underscore ('_').
+
+Examples:
+
+* 'abc' =>  ['ab', 'c_']
+* 'abcdef' => ['ab', 'cd', 'ef']
+ */
+
+function solution(str){
+  if (str.length % 2) str += '_';
+  const arr = [];
+  for (let i = 0; i < str.length; i += 2) {
+    arr.push(str.slice(i, i + 2));
+  }
+  return arr;
+}
