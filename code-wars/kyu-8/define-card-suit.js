e716dbb298d2aef4b0a933ef7bde838b3658e751
Caleb Phillips <csphillips5@gmail.com>
2022-04-19 11:36:31 -0400

    submit extra coding challenge

---
+++ code-wars/kyu-8/define-card-suit.js
@@ -0,0 +1,23 @@
+/**
+ * You get any card as an argument. Your task is to return the suit of this card (in lowercase).
+
+Our deck (is preloaded):
+
+deck = ['2♣','3♣','4♣','5♣','6♣','7♣','8♣','9♣','10♣','J♣','Q♣','K♣','A♣',
+        '2♦','3♦','4♦','5♦','6♦','7♦','8♦','9♦','10♦','J♦','Q♦','K♦','A♦',
+        '2♥','3♥','4♥','5♥','6♥','7♥','8♥','9♥','10♥','J♥','Q♥','K♥','A♥',
+        '2♠','3♠','4♠','5♠','6♠','7♠','8♠','9♠','10♠','J♠','Q♠','K♠','A♠'];
+('3♣') -> return 'clubs'
+('3♦') -> return 'diamonds'
+('3♥') -> return 'hearts'
+('3♠') -> return 'spades'
+ */
+
+function defineSuit(card) {
+  switch(card[card.length - 1].charCodeAt()) {
+    case 9824: return 'spades';
+    case 9827: return 'clubs';
+    case 9829: return 'hearts';
+    case 9830: return 'diamonds';
+  }
+}
