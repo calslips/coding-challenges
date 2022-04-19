b43fac66fe33ab71ba4d10f58447587fd5bfd600
Caleb Phillips <csphillips5@gmail.com>
2022-04-19 11:50:02 -0400

    submit extra coding challenge

---
+++ code-wars/kyu-8/ascii-total.js
@@ -0,0 +1,12 @@
+/**
+ * You'll be given a string, and have to return the sum of all characters
+ * as an int. The function should be able to handle all ASCII characters.
+
+examples:
+
+uniTotal("a") == 97 uniTotal("aaa") == 291
+ */
+
+function uniTotal(string) {
+  return string.split('').reduce((s, c) => s + c.charCodeAt(), 0);
+}
