a4534c5332c6d0f9b7b22cca08710ffe8f21588c
Caleb Phillips <csphillips5@gmail.com>
2022-04-22 12:38:48 -0400

    submit extra coding challenge

---
+++ code-wars/kyu-8/barking-mad.js
@@ -0,0 +1,21 @@
+/**
+ * Teach snoopy and scooby doo how to bark using object methods.
+ * Currently only snoopy can bark and not scooby doo.
+
+snoopy.bark(); // return "Woof"
+scoobydoo.bark(); // undefined
+Use method prototypes to enable all Dogs to bark.
+ */
+
+class Dog {
+  constructor(breed) {
+    breed;
+  }
+  bark() {
+    return 'Woof';
+  }
+}
+
+var snoopy = new Dog("Beagle");
+
+var scoobydoo = new Dog("Great Dane");
