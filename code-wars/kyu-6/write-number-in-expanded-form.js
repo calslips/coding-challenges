f2f30884200f525de07bd64aef079f6140b3b49f
Caleb Phillips <csphillips5@gmail.com>
2022-04-22 10:25:02 -0400

    submit extra coding challenge

---
+++ code-wars/kyu-6/write-number-in-expanded-form.js
@@ -0,0 +1,21 @@
+/**
+ * You will be given a number and you will need to return it as a string
+ * in Expanded Form. For example:
+
+expandedForm(12); // Should return '10 + 2'
+expandedForm(42); // Should return '40 + 2'
+expandedForm(70304); // Should return '70000 + 300 + 4'
+NOTE: All numbers will be whole numbers greater than 0.
+ */
+
+function expandedForm(num) {
+  num = num.toString();
+  let expanded = [];
+  while (num) {
+    if (+num[0]) {
+      expanded.push(num[0].padEnd(num.length, '0'));
+    }
+    num = num.slice(1);
+  }
+  return expanded.join(' + ');
+}
