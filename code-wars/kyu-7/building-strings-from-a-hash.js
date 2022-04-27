9c09d32d499b6fc2d53322d24fb28581e1c53131
Caleb Phillips <csphillips5@gmail.com>
2022-04-27 17:24:22 -0400

    submit extra coding challenge

---
+++ code-wars/kyu-7/building-strings-from-a-hash.js
@@ -0,0 +1,24 @@
+/**
+ * Complete the solution so that it takes the object (JavaScript/CoffeeScript)
+ * or hash (ruby) passed in and generates a human readable string from its
+ * key/value pairs.
+
+The format should be "KEY = VALUE". Each key/value pair should be separated
+by a comma except for the last pair.
+
+Example:
+
+solution({a: 1, b: '2'}) // should return "a = 1,b = 2"
+ */
+
+function solution(pairs){
+  return Object.entries(pairs).map(([key, value]) => `${key} = ${value}`).join(',');
+}
+
+// function solution(pairs){
+//   let props = []
+//   for (let [key, value] of Object.entries(pairs)) {
+//     props.push(`${key} = ${value}`);
+//   }
+//   return props.join(',');
+// }
