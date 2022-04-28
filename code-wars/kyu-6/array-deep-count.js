c579e4bdbff1b1fb17864616412997c113faae03
Caleb Phillips <csphillips5@gmail.com>
2022-04-28 10:32:40 -0400

    submit daily coding challenge

---
+++ code-wars/kyu-6/array-deep-count.js
@@ -0,0 +1,29 @@
+/**
+ * Array.prototype.length will give you the number of top-level elements
+ * in an array.
+
+Your task is to create a function deepCount that returns the number of ALL
+elements within an array, including any within inner-level arrays.
+
+For example:
+
+deepCount([1, 2, 3]);
+//>>>>> 3
+deepCount(["x", "y", ["z"]]);
+//>>>>> 4
+deepCount([1, 2, [3, 4, [5]]]);
+//>>>>> 7
+The input will always be an array.
+ */
+
+function deepCount(a) {
+  let count = a.length;
+  for (let n of a) {
+    if (Array.isArray(n)) count += deepCount(n);
+  }
+  return count;
+}
+
+// function deepCount(a) {
+//   return a.reduce((c, n) => Array.isArray(n) ? c + deepCount(n) : c, a.length);
+// }
