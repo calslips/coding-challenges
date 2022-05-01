a71cef36111fcfbd238379158bfc111294c11cd1
Caleb Phillips <csphillips5@gmail.com>
2022-05-01 11:32:25 -0400

    submit extra coding challenge

---
+++ code-wars/kyu-6/is-integer-array.js
@@ -0,0 +1,19 @@
+/**
+ * Write a function with the signature shown below:
+
+function isIntArray(arr) {
+  return true
+}
+returns true / True if every element in an array is an integer or a float
+with no decimals.
+returns true / True if array is empty.
+returns false / False for every other input.
+ */
+
+function isIntArray(arr) {
+  return !!arr && arr.every(e => Math.trunc(e) === e);
+}
+
+// function isIntArray(arr) {
+//   return arr ? arr.every(e => Math.trunc(e) === e) : false;
+// }
