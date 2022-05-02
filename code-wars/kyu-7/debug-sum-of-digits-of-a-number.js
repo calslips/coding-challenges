4578569e70355c48a34a5e535e351eea4694b326
Caleb Phillips <csphillips5@gmail.com>
2022-05-02 09:08:46 -0400

    submit daily coding challenge

---
+++ code-wars/kyu-7/debug-sum-of-digits-of-a-number.js
@@ -0,0 +1,18 @@
+/**
+ * Debug   function getSumOfDigits that takes positive integer to calculate
+ * sum of it's digits. Assume that argument is an integer.
+
+Example
+123  => 6
+223  => 7
+1337 => 14
+ */
+
+function getSumOfDigits(integer) {
+  var sum = 0;
+  var digits = integer.toString();
+  for(var ix = 0; ix < digits.length; ix++) {
+    sum += +digits[ix];
+  }
+  return sum;
+}
