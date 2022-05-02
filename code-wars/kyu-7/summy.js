f985645c4b1aa336c04bc3e541274362f8b57b1a
Caleb Phillips <csphillips5@gmail.com>
2022-05-01 20:15:00 -0400

    submit extra coding challenge

---
+++ code-wars/kyu-7/summy.js
@@ -0,0 +1,12 @@
+/**
+ * Write a function that takes a string which has integers inside it
+ * separated by spaces, and your task is to convert each integer in
+ * the string into an integer and return their sum.
+
+Example
+summy("1 2 3")  ==> 6
+ */
+
+function summy(stringOfInts) {
+  return stringOfInts.split(' ').reduce((s, n) => s + +n, 0);
+}
