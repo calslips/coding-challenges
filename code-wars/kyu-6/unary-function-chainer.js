bf7be0ccd05e74bc267f0eb97a4f70aeb0ae296d
Caleb Phillips <csphillips5@gmail.com>
2022-07-11 11:12:17 -0400

    submit daily coding challenge

---
+++ code-wars/kyu-6/unary-function-chainer.js
@@ -0,0 +1,13 @@
+/**
+ * Your task is to write a higher order function for chaining together a
+ * list of unary functions. In other words, it should return a function
+ * that does a left fold on the given functions.
+
+chained([a,b,c,d])(input)
+Should yield the same result as
+d(c(b(a(input))))
+ */
+
+function chained(functions) {
+  return a => functions.reduce((x, f) => f(x), a);
+}
