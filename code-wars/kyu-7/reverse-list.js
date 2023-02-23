c7891aaaaf6c97aa6416c7bba36dff3cd9218678
Caleb Phillips <csphillips5@gmail.com>
2023-02-23 15:47:40 -0500

    submit extra coding challenge

---
+++ code-wars/kyu-7/reverse-list.js
@@ -0,0 +1,16 @@
+/*
+Write reverseList function that simply reverses lists.
+
+Parameters: an array
+Returns: an array
+  elements in reverse order from input
+Examples:
+  reverseList([]), []
+  reverseList([1,2,3]), [3,2,1]
+Pseudocode:
+  return result of reverse method called on input array
+*/
+
+function reverseList(arr) {
+  return arr.reverse();
+}
