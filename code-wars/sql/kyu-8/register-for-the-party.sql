dda33c7f4d367297e469e8e7022709493baa9ec8
Caleb Phillips <csphillips5@gmail.com>
2023-04-09 14:53:43 -0400

    submit extra coding challenge

---
+++ code-wars/sql/kyu-8/register-for-the-party.sql
@@ -0,0 +1,14 @@
+-- You received an invitation to an amazing party. Now you need to write an
+-- insert statement to add yourself to the table of participants.
+
+-- participants table schema
+-- name (string)
+-- age (integer)
+-- attending (boolean)
+
+-- NOTES:
+-- Since alcohol will be served, you can only attend if you are 21 or older
+-- You can't attend if the attending column returns anything but true
+
+INSERT INTO participants VALUES ('Lochness Monster', 350, true);
+SELECT * FROM participants;
