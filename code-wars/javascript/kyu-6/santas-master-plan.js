7c2c5b4814c9347ebb765c452f69479015dc0c94
Caleb Phillips <csphillips5@gmail.com>
2023-04-10 09:09:15 -0400

    submit daily coding challenge

---
+++ code-wars/javascript/kyu-6/santas-master-plan.js
@@ -0,0 +1,65 @@
+/*
+Santa has just revealed his master plan! His goal was to
+automate as many of his tasks as possible by posting them as
+challenges in the #hackingholidays section of Codewars. Thanks
+to the solutions from these challenges, Santa has completed his
+tasks a week before Christmas! You know what that means...Party
+at Santa's place! All that's left is to invite everyone. Santa
+sent out a large amount of invitations, and is patiently waiting
+for responses.
+
+Who's attending the Party?
+Write a function called getAttendees that takes two arguments:
+
+Array containing all the names of the people Santa invited
+An array of responses (objects) with the following structure:
+{name: 'Easter Bunny', response: 'declined'}
+
+getAttendees should return an array containing the names of all
+the people who accepted Santa's invitation and the names of
+those who did not respond to the invitation. Santa is very
+positive, so he is assuming those who did not respond will show
+up. Anyone who declined the invitation will not be attending the
+party. If nobody is attending the party, return an empty array [].
+
+Example:
+getAttendees( ['Easter Bunny', 'Tooth Fairy', 'Frosty the Snowman', 'Jack Frost'] ,
+   [
+     {name: 'Easter Bunny', response: 'declined'},
+     {name: 'Jack Frost', response: 'declined'},
+     {name: 'Tooth Fairy', response: 'accepted'}
+   ] );// => returns ['Tooth Fairy', 'Frosty the Snowman']
+
+Parameters:
+  an array of strs
+    names of invited
+  an array of objs
+    name, str
+    response, str
+Returns:
+  an array of strs
+    names of attending
+Examples:
+  getAttendees(
+    ['Easter Bunny', 'Tooth Fairy', 'Frosty the Snowman', 'Jack Frost', 'Cupid', 'Father Time'],
+    [
+     {name: 'Easter Bunny', response: 'declined'},
+     {name: 'Jack Frost', response: 'declined'},
+     {name: 'Tooth Fairy', response: 'accepted'}
+    ]
+  ),
+  ['Tooth Fairy', 'Frosty the Snowman', 'Cupid', 'Father Time']
+Pseudocode:
+  initialize a noGo var assigned to an empty obj
+  iterate over the response array
+    if current el's response is declined
+      store name in noGo as key, with true as value
+  filter people array
+    only keep names that are not in the noGo obj
+  return filtered array
+*/
+
+function getAttendees(peopleInvited, responses) {
+  const noGo = responses.reduce((o, p) => (p.response === 'declined' && (o[p.name] = 1), o), {});
+  return peopleInvited.filter(p => !noGo[p]);
+}
