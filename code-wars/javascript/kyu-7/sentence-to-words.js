9a980de806ab4053c06fb1fc76bc1c381e0299b5
Caleb Phillips <csphillips5@gmail.com>
2023-07-17 07:43:48 -0400

    submit daily coding challenge

---
+++ code-wars/javascript/kyu-7/sentence-to-words.js
@@ -0,0 +1,13 @@
+/*
+Write function splitSentence which will create a list of strings
+from a string.
+
+Parameters: a str
+Returns: an array of strs
+Examples:
+  splitSentence('hello world'), ['hello', 'world']
+Pseudocode:
+  return input str split on spaces
+*/
+
+const splitSentence = s => s.split(' ');
